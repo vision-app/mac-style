398376c09b98e0b4077339911fcbc0252a0c36e8
Rainfore <rainforest92@126.com>
2017-10-30 10:24:04 +0800

    :wrench: Add libraryPath in config

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './u-base.vue/base.css';
+import Base from './u-base.vue';
+import Button from './u-button.vue';
+import Capsules from './u-capsules.vue';
+import Capsule from './u-capsule.vue';
+import ColorPicker from './u-color-picker.vue';
+import Collapse from './u-collapse.vue';
+import CollapseItem from './u-collapse-item.vue';
+import LinearLayout from './u-linear-layout.vue';
+import Link from './u-link.vue';
+import ListView from 'u-list-view.vue';
+import ListViewItem from './u-list-view-item.vue';
+import Pallette from './u-pallette.vue';
+import Panel from './u-panel.vue';
+import Tab from 'u-tab.vue';
+import TabView from './u-tab-view.vue';
+import TabViewItem from './u-tab-view-item.vue';
+import Tabs from './u-tabs.vue';
+import Titlebar from './u-titlebar.vue';
+import TreeView from 'u-tree-view.vue';
+import TreeViewNode from './u-tree-view-node.vue';
+import TreeViewText from 'u-tree-view-text.vue';
+
+export {
+    Base,
+    Button,
+    Capsules,
+    Capsule,
+    Collapse,
+    CollapseItem,
+    ColorPicker,
+    LinearLayout,
+    Link,
+    ListView,
+    ListViewItem,
+    Pallette,
+    Panel,
+    Tab,
+    TabView,
+    TabViewItem,
+    Tabs,
+    Titlebar,
+    TreeView,
+    TreeViewNode,
+    TreeViewText,
+};
